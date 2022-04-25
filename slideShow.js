bc6b0088b1a50bb6f0dd0c408fe63c2eb1793baa
hwang sung won <sungweni33@naver.com>
2022-04-25 21:33:40 +0900

    22/04/25 21:33

--- slideShow.js
+++ slideShow.js
@@ -4,11 +4,10 @@ let currentIdx = 0;
 const slideCount = slidetxt.length;
 const prev = document.querySelector('.prev'); //이전 버튼 
 const next = document.querySelector('.next'); //다음 버튼 
-
-const slideWidth = document.querySelector('.slideShow').offsetWidth; //한개의 슬라이드 넓이 
 const slideMargin = 0; //슬라이드간의 margin 값
 
 
+const slideWidth = document.querySelector('.slideShow').offsetWidth; //한개의 슬라이드 넓이 
 slides.style.width = (slideWidth + slideMargin) * slideCount + 5 + 'px';
 
 function moveSlide(num) {
@@ -26,20 +25,20 @@ next.addEventListener('click', function () {
     }
 })
 
-const IdAndPw = document.querySelectorAll('.login');
-const login = document.querySelector('#login_submit');
+// const IdAndPw = document.querySelectorAll('.login');
+// const login = document.querySelector('#login_submit');
 
-function loginCheck(){
-    login_info = document.getElementById('login_fault');
-    if(IdAndPw[0].value == 'abcd' && IdAndPw[1].value == '1234'){
-        location.href='index.html';
-    }
-    else{
-        login_info.innerHTML = "아이디 또는 패스워드가 올바르지 않습니다";
-        IdAndPw[0].value='';
-        IdAndPw[1].value='';
-    }
-}
-login.addEventListener('click', function(){
-    loginCheck();
-})
+// function loginCheck(){
+//     login_info = document.getElementById('login_fault');
+//     if(IdAndPw[0].value == 'abcd' && IdAndPw[1].value == '1234'){
+//         location.href='index.html';
+//     }
+//     else{
+//         login_info.innerHTML = "아이디 또는 패스워드가 올바르지 않습니다";
+//         IdAndPw[0].value='';
+//         IdAndPw[1].value='';
+//     }
+// }
+// login.addEventListener('click', function(){
+//     loginCheck();
+// })
